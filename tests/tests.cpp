995de3fca5a8590745b83739fa7f926a20b82faa
CarryUa <mosuk81@gmail.com>
2026-02-25 23:27:41 +0100

    Adding test submodule

---
+++ tests/tests.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+namespace
+{
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+TEST(FooTest, FooSubtest)
+{
+    EXPECT_EQ(add(1, 2), 3);
+    EXPECT_EQ(add(10, 20), 30);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
