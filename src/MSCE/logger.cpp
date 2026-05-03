954200ccaebfc9680f9f3bb5f58a394ee208b589
CarryUa <mosuk81@gmail.com>
2026-05-03 23:50:22 +0200

    oopsss

---
+++ src/MSCE/logger.cpp
@@ -0,0 +1,34 @@
+#include "logger.h"
+#include <filesystem>
+#include <chrono>
+
+using namespace msce;
+
+std::size_t Logger::millis_at_start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+
+std::ofstream Logger::log_file = std::ofstream(std::filesystem::temp_directory_path() / "msce_log.txt");
+
+void Logger::append_to_log_file(const std::string_view &message) const noexcept
+{
+    if (!this->log_file.is_open())
+    {
+        std::cerr << "[ERROR]: [LOGGER]: Failed to create output file!" << std::endl;
+        return;
+    }
+
+    std::size_t cur_t = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+
+    log_file << "[" << (double)(cur_t - millis_at_start) / 1000 << "s] | " << message.data() << "\n";
+}
+
+msce::Logger::Logger(const std::string &owner_name)
+{
+    if (this->millis_at_start == 0)
+        this->millis_at_start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+
+    this->owner_name = owner_name;
+}
+Logger::~Logger()
+{
+    this->log_file.close();
+}
--- src/MSCE/logger.h
+++ src/MSCE/logger.h
@@ -3,9 +3,7 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include <filesystem>
 #include <format>
-#include <chrono>
 
 namespace msce
 {
@@ -31,35 +29,6 @@ namespace msce
         template <typename... Args>
         void log_debug(const std::string &message, Args... args) const;
     };
-
-    inline std::size_t Logger::millis_at_start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-
-    inline std::ofstream Logger::log_file = std::ofstream(std::filesystem::temp_directory_path() / "msce_log.txt");
-
-    inline void Logger::append_to_log_file(const std::string_view &message) const noexcept
-    {
-        if (!this->log_file.is_open())
-        {
-            std::cerr << "[ERROR]: [LOGGER]: Failed to create output file!" << std::endl;
-            return;
-        }
-
-        std::size_t cur_t = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-
-        log_file << "[" << (double)(cur_t - millis_at_start) / 1000 << "s] | " << message.data() << "\n";
-    }
-
-    inline msce::Logger::Logger(const std::string &owner_name)
-    {
-        if (this->millis_at_start == 0)
-            this->millis_at_start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-
-        this->owner_name = owner_name;
-    }
-    inline Logger::~Logger()
-    {
-        this->log_file.close();
-    }
 
     template <typename... Args>
     inline void Logger::log_info(const std::string &message, Args... args) const
@@ -93,6 +62,7 @@ namespace msce
         return;
 #endif
     }
+
 }
 
 #endif
